8d2a161a7916953e0c94923177da8c071065478d
Carl Xu <xudifsd@gmail.com>
2013-09-15 12:59:52 +0800

    format it first, so we can makes diff more useful

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,14 +1,13 @@
 package control;
 
-public class Control
-{
-  // compiler testing and debugging
-  public static boolean testlexer = false;
-  
-  // lexer and parser
-  public static boolean lex = false;
+public class Control {
+	// compiler testing and debugging
+	public static boolean testlexer = false;
 
-  // elaborator
-  public static boolean elabClassTable = false;
-  public static boolean elabMethodTable = false;
+	// lexer and parser
+	public static boolean lex = false;
+
+	// elaborator
+	public static boolean elabClassTable = false;
+	public static boolean elabMethodTable = false;
 }
--- src/util/Flist.java
+++ src/util/Flist.java
@@ -1,17 +1,14 @@
 package util;
 
-public class Flist<X>
-{
-  public Flist()
-  {
-  }
+public class Flist<X> {
+	public Flist() {
+	}
 
-  public java.util.LinkedList<X> addAll(
-      @SuppressWarnings("unchecked") X... args)
-  {
-    java.util.LinkedList<X> list = new java.util.LinkedList<X>();
-    for (X arg : args)
-      list.addLast(arg);
-    return list;
-  }
+	public java.util.LinkedList<X> addAll(
+			@SuppressWarnings("unchecked") X... args) {
+		java.util.LinkedList<X> list = new java.util.LinkedList<X>();
+		for (X arg : args)
+			list.addLast(arg);
+		return list;
+	}
 }
